df233d29db23c600c9fd442c738af09d90e08b63
Shamil <kurbanov@outlook.de>
2023-08-01 15:04:41 +0200

    Hello Console app edit

--- Examples/Example001_HelloConsole/Program.cs
+++ Examples/Example001_HelloConsole/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console; //using static directive
+
+namespace Example001_HelloConsole
+{
+    class Program
+    { //start of class
+        static void Main(string[] args)
+        {
+            WriteLine("Hello Console");
+            ReadKey();
+        }
+    } //end of class
+} //end of namespace
+
+// Path: Examples\Example002_HelloConsole\Program.cs
