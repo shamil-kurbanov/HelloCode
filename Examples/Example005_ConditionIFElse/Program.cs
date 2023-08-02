c492c5e98a0057cf827d8c7af415fa2477a46cfb
Shamil <kurbanov@outlook.de>
2023-08-02 11:33:39 +0200

    add username function

--- Examples/Example005_ConditionIFElse/Program.cs
+++ Examples/Example005_ConditionIFElse/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Code snippet: Example005_ConditionIFElse\Program.cs
+Console.WriteLine("Please enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "linda")
+{
+    Console.WriteLine("Welcome Linda!");
+}
+else
+{
+    Console.WriteLine("Welcome " + username);
+}
+
+Console.ReadLine(); 
