f3b16b479fac0c050c85e895213d7e20d8e6483d
Shamil <kurbanov@outlook.de>
2023-08-02 11:13:04 +0200

    add "random"

--- Examples/Example003_Sum/Program.cs
+++ Examples/Example003_Sum/Program.cs
@@ -1,5 +1,16 @@
-﻿int numberA = 10;
-int numberB = 5;
-int sum = numberA + numberB;
-Console.WriteLine("The sum of {0} and {1} is {2}", numberA, numberB, sum);
+﻿int numberA = new Random().Next(1, 10); // Random number between 1 and 
+int numberB = new Random().Next(1, 10); // Random number between 1 and 10
+
+int sum = numberA + numberB;        // Sum of two numbers
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("The sum of {0} and {1} is {2}", numberA, numberB, sum);  // Print the sum
+    Console.WriteLine("Example Number: ", i); // Print the example number
+} // 
+
+// Wait for user to press the Enter key
+Console.ReadLine();
+
+// Path: Examples\Example004_Div\Program.cs 
 
