6fe412641a7f70d8187951e69ecd10b6cff34e9b
Shamil <kurbanov@outlook.de>
2023-08-03 10:43:01 +0200

    add Example009_IntroArray_2

---
+++ Examples/Example009_IntroArray_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console; 
+
+Clear();
+
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int find=18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        WriteLine("index: " + index 
+        + " array[index]: " + array[index]);
+        break;
+    }
+
+    index++;
+}
