67b3ff12a6ae50b33a7144e3d2bbfef135de1dab
Shamil <kurbanov@outlook.de>
2023-08-03 10:16:59 +0200

    add "Example008_IntroMethod"

---
+++ Examples/Example008_IntroMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console; 
+
+Clear();
+
+int Max(int a, int b, int c)
+{
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+// WriteLine("max1: "+max1);
+// WriteLine("max2: "+max2);
+// WriteLine("max3: "+max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3)
+    );
+WriteLine("max: "+max);
